3e9815e944828cdf275db57367f0e0c0fbc2e623
monicasangam12 <47582209+monicasangam12@users.noreply.github.com>
2025-10-24 14:57:03 -0400

    Refactor SearchEmployeeSkillsComponent with Reactive Forms

--- src/app/search-employee-skills/search-employee-skills.ts
+++ src/app/search-employee-skills/search-employee-skills.ts
@@ -1,30 +1,26 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, FormControl } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormField } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { EmployeeService } from '../employee-service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-employee-skills',
-  imports: [FormsModule],
-  providers: [HttpClient, EmployeeService],
-  standalone: true,
+  imports: [MatFormField, MatSelectModule, MatOptionModule, ReactiveFormsModule, FormsModule],
   templateUrl: './search-employee-skills.html',
-  styleUrl: './search-employee-skills.css'
+  styleUrls: ['./search-employee-skills.css'],
 })
 export class SearchEmployeeSkillsComponent {
-  searchText: string = '';
+  skillsControl = new FormControl('');
+  skillsList: string[] = ['Java', 'Angular', 'Python', 'C#', 'SQL', 'AWS'];
 
-  constructor(private employeeService: EmployeeService) {
-    this.employeeService = employeeService;
-  }
+  constructor(private employeeService: EmployeeService) {}
 
-  onSearch(event: any) {
-    const query = event.target.value.toLowerCase();
-    console.log('Searching for employees with skill:', query);
-
-    this.employeeService.findBySearchText(query).subscribe((results) => {
-      console.log('Search results:', results);
+  searchSkills(skill: string) {
+    console.log("Searching employees with skill: ", skill);
+    this.employeeService.findBySearchText(skill).subscribe((results: any) => {
+      console.log("Search results: ", results);
     });
   }
-
 }
