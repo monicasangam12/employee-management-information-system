559f5c5eceb542a668353762502f682478b47819
monicasangam12 <47582209+monicasangam12@users.noreply.github.com>
2025-10-28 10:51:10 -0400

    Refactor state machine actions and current state handling

--- src/app/app.statemachine.ts
+++ src/app/app.statemachine.ts
@@ -15,23 +15,24 @@ export enum STATE{
 
 export enum ACTION { 
         /* Buttons-Submits/Verbs*/
-    NONE,
-    GO_REGISTER,
-    SUBMIT_REGISTER,
-    GO_LOGIN,
-    SUBMIT_LOGIN,
-    GO_DASHBOARD,
-    GO_SEARCH,
-    SUBMIT_SEARCH,
-    SUBMIT_FILTER,
-    SUBMIT_SORT,
-    GO_ADD,
-    SUBMIT_ADD,
-    GO_EDIT,
-    SUBMIT_EDIT,
-    GO_DELETE,
-    SUBMIT_DELETE,
-    LOGOUT
+    NONE,                       //0
+    GO_REGISTER,                //1
+    SUBMIT_REGISTER,            //2
+    GO_LOGIN,                   //3
+    SUBMIT_LOGIN,               //4
+    GO_DASHBOARD,               //5
+    SUBMIT_SELECT,              //6
+    SUBMIT_UNSELECT,            //7
+    GO_SEARCH,                  //8
+    SUBMIT_SEARCH,              //9
+    SUBMIT_FILTER,              //10
+    SUBMIT_SORT,                //11
+    GO_ADD,                     //12
+    SUBMIT_ADD,                 //13
+    GO_EDIT,                    //14
+    SUBMIT_EDIT,                //15
+    SUBMIT_DELETE,              //16
+    LOGOUT                      //17    
  }
  export interface STATE_MACHINE_STEP {
     from:STATE,
@@ -39,28 +40,35 @@ export enum ACTION {
     action:ACTION
  }
 
- const currentMachineStep:STATE_MACHINE_STEP = { from: STATE.HOME, to: STATE.HOME, action: ACTION.NONE };
+ 
 
 const stateMachine =  [
   { from: STATE.HOME, to: STATE.REGISTER, action: ACTION.GO_REGISTER },
   { from: STATE.LOGIN, to: STATE.REGISTER, action: ACTION.GO_REGISTER },
-  { from: STATE.REGISTER, to: currentMachineStep.from, action: ACTION.SUBMIT_REGISTER },
-
+  { from: STATE.REGISTER, to: STATE.HOME, action: ACTION.SUBMIT_REGISTER },
+  { from: STATE.REGISTER, to: STATE.LOGIN, action: ACTION.SUBMIT_REGISTER },
   { from: STATE.HOME, to: STATE.LOGIN, action: ACTION.GO_LOGIN },
+
+  { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.DASHBOARD_ONE_SELECTION, action: ACTION.SUBMIT_SELECT },
+  { from: STATE.DASHBOARD_ONE_SELECTION, to: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, action: ACTION.SUBMIT_SELECT },
+  { from: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, to: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, action: ACTION.SUBMIT_SELECT },
+  { from: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, to: STATE.DASHBOARD_ONE_SELECTION, action: ACTION.SUBMIT_UNSELECT },
+  { from: STATE.DASHBOARD_ONE_SELECTION, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_UNSELECT },
 
   { from: STATE.LOGIN, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_LOGIN },
 
   { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_SORT },
   { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_FILTER },
-  { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_DELETE }, 
+
   { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.SEARCH, action: ACTION.GO_SEARCH },
   { from: STATE.SEARCH, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_SEARCH },
   { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.ADD, action: ACTION.GO_ADD },
   { from: STATE.ADD, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_ADD },
-  { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.EDIT, action: ACTION.GO_EDIT }, 
-  { from: STATE.EDIT, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_EDIT },
+
+
   { from: STATE.DASHBOARD_NO_SELECTION, to: STATE.HOME, action: ACTION.LOGOUT },
 
+  { from: STATE.DASHBOARD_ONE_SELECTION, to: STATE.DASHBOARD_NO_SELECTION, action: ACTION.SUBMIT_UNSELECT },
 
   { from: STATE.DASHBOARD_ONE_SELECTION, to: STATE.DASHBOARD_ONE_SELECTION, action: ACTION.SUBMIT_SORT },
   { from: STATE.DASHBOARD_ONE_SELECTION, to: STATE.DASHBOARD_ONE_SELECTION, action: ACTION.SUBMIT_FILTER },
@@ -80,8 +88,8 @@ const stateMachine =  [
   { from: STATE.SEARCH, to: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, action: ACTION.SUBMIT_SEARCH },
   { from: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, to: STATE.ADD, action: ACTION.GO_ADD },
   { from: STATE.ADD, to: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, action: ACTION.SUBMIT_ADD },
-  { from: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, to: STATE.EDIT, action: ACTION.GO_EDIT }, 
-  { from: STATE.EDIT, to: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, action: ACTION.SUBMIT_EDIT },
+
+
   { from: STATE.DASHBOARD_MORE_THAN_ONE_SELECTION, to: STATE.HOME, action: ACTION.LOGOUT },
 ];
 
@@ -91,7 +99,7 @@ const stateMap = new Map([
     [STATE.LOGIN, 'login'],
     [STATE.EDIT, 'edit-employee/'],
     [STATE.SEARCH, 'search-employees-skill'],
-    [STATE.DASHBOARD_NO_SELECTION, 'employees/'],
+    [STATE.DASHBOARD_NO_SELECTION, 'employees'],
    
 
 ])
@@ -100,24 +108,44 @@ const stateMap = new Map([
     providedIn: 'root'
 })
 export class StateMachineService {
+
+    currentMachineStep:STATE_MACHINE_STEP = { from: STATE.HOME, to: STATE.HOME, action: ACTION.NONE };
+
     getCurrentState() {
-        return currentMachineStep.from;
+        return this.currentMachineStep.to;
     }
 
     getNextState(action: ACTION) {
+        console.log(this.currentMachineStep);
         let nextState:STATE = STATE.NONE;
         stateMachine.forEach(step => {
-            if(currentMachineStep.from == step.from && action == step.action)
+            if(this.currentMachineStep.from == step.from && action == step.action)
                 nextState = step.to;
         })
         return stateMap.get(nextState)
     }
 
+    goNextState(action: ACTION) {
+        console.log("currentState = ", this.currentMachineStep);
+        let nextState:STATE = STATE.NONE;
+        let stepFound!:STATE_MACHINE_STEP;
+        stateMachine.forEach(step => {
+            if(this.currentMachineStep.to == step.from && action == step.action) {
+                stepFound = step;
+                nextState = step.to;
+            }
+                
+        })
+        this.currentMachineStep = stepFound;
+        console.log("new currentState = ", this.currentMachineStep);
+        return stateMap.get(nextState)
+    }
+
     getPageActions() {
-        currentMachineStep.from;
+        this.currentMachineStep.from;
         let actionList:ACTION[] = [];
         stateMachine.forEach(step => {
-            if(currentMachineStep.from == step.from )
+            if(this.currentMachineStep.to == step.from )
                 actionList.push(step.action)
         })
         return actionList;
